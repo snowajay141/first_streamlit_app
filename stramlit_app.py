abd08802229050cb421f13c0a1403ea4c3d6f778
snowajay141 <131150578+snowajay141@users.noreply.github.com>
2023-04-18 17:34:19 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -5,6 +5,7 @@ streamlit.header('Breakfast MENU')
 streamlit.text('Omega3 & Blueberry Oatmeal')
 streamlit.text('Kale Spinach & Rocket Smoothee')
 streamlit.text('Hard boiled free range egg')
+streamlit.header('Build your own fruit Smoothies')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
