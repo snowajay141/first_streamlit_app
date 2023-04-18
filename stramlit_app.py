45fb7092eef71e99a0e1427cace2b56e7fb31ccf
snowajay141 <131150578+snowajay141@users.noreply.github.com>
2023-04-18 17:29:03 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
-streamlit.header('Breakfast MENU')
+streamlit.header(':-) Breakfast MENU')
 streamlit.text('Omega3 & Blueberry Oatmeal')
 streamlit.text('Kale Spinach & Rocket Smoothee')
 streamlit.text('Hard boiled free range egg')
